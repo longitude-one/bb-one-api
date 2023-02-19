b713b9a7e6a634b05388f797fba1be5cc18a15c4
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2023-02-19 11:49:20 +0100

    * Foundry bundle added * Fixtures bundle added

---
+++ api/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the BB-One Project
+ *
+ * PHP 8.2 | Symfony 6.2+
+ *
+ * Copyright LongitudeOne - Alexandre Tranchant
+ * Copyright 2023 - 2023
+ *
+ */
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
